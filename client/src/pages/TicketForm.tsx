da699543de216dc7f14c4dbb229761f223c79c75
Luis Felipe <igorspopovich@hotmail.com>
2025-11-10 14:53:11 -0800

    Diseno optimizado para movil

--- client/src/pages/TicketForm.tsx
+++ client/src/pages/TicketForm.tsx
@@ -217,7 +217,15 @@ export default function TicketForm() {
                     <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                   </Button>
                 </PopoverTrigger>
-                <PopoverContent className="w-[400px] p-0" align="start">
+                <PopoverContent 
+                  className="w-[calc(100vw-2rem)] max-w-[400px] p-0" 
+                  align="start"
+                  side="bottom"
+                  sideOffset={8}
+                  avoidCollisions={true}
+                  collisionPadding={{ top: 16, bottom: 200, left: 16, right: 16 }}
+                  sticky="always"
+                >
                   <Command shouldFilter={false}>
                     <CommandInput
                       placeholder="Buscar cliente..."
